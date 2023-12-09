35e03988eba3e33f73ad4102dddabf9a06c0b7a5
Ricardo_Sao <99063411+RicardoRobledo@users.noreply.github.com>
2023-12-09 15:13:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
   res.write('You are  in AWS  Cloud'); //write a response to the client
-  res.end(); //end the response
+  res.end();
 }).listen(4000);
